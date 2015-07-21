1188384b8b6fc2e50f860cdb0ee369283b80a68e
jourdanj <jourdanjoyner@gmail.com>
2015-07-20 21:57:09 -0700

    Signed-off-by: jourdanj <jourdanjoyner@gmail.com>

---
+++ Project/main.cpp
@@ -0,0 +1,172 @@
+/* 
+ * File:   main.cpp
+ * Author: Jourdan Joyner
+ * Created on July 17, 2015, 3:43 PM
+ * Purpose: C++ Project
+ */
+
+//file libraries
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+#define GAMEFILE "world.txt"//read in for the world data
+#define CONTINUE    1
+#define QUIT            0
+
+//user libraries
+struct tArea{//structure that holds the info for the main area
+    
+    string strCurrentArea;//name of current area
+    string strDescription;//description of current area
+    string strNorth;//name of area to the north
+    string strSouth;//name of area to the south
+    string strEast;//name of area to the east
+    string strWest;//name of area to the west
+    
+};
+//global constants
+
+//function prototypes
+void DisplayArea(tArea &area);
+void GetAreaInfo(ifstream&,tArea &area);
+void Move(ifstream&,tArea &area,string);
+int Input(ifstream&,tArea &area);
+//execution
+int main(){
+    
+    ifstream fin;//pointer that opens and reads from file
+    tArea area;//area structure data
+    
+    fin.open(GAMEFILE);//
+    
+    if(fin.fail()){
+        cout<<"UNABLE TO FIND GAME FILE\n";
+        
+        return -1;
+    }
+    fin>>area.strCurrentArea>>area.strCurrentArea;//reads in the start point for the game
+    
+    GetAreaInfo(fin,area);//calls function to receive info from the file
+    
+    DisplayArea(area);//calls function to display the area to the user
+    
+    while(1){//main game loop
+        
+            
+        if(Input(fin,area)==QUIT){//if user enters quit, game ends
+
+            break;
+        }
+    }
+    
+    fin.close();//closes file
+    
+    return 0;
+}
+/**********************************************************/
+//*****************DisplayArea*****************************/
+//******Function is called to display area description*****/
+/**********************************************************/
+void DisplayArea(tArea &area)
+{
+    cout<<area.strDescription<<endl<<endl;		
+}
+/**********************************************************/
+//*****************GetAreaInfo*****************************/
+//******Function is called to read in the area*************/
+//************information from the file********************/
+/**********************************************************/
+void GetAreaInfo(ifstream &fin,tArea &area){
+    
+    string strTemp="";
+    string strTemp2="";//temporary for reading in info
+    
+    string strArea="<"+area.strCurrentArea+">";//looks for the room name in brackets to find easier i.e instead of main it reads in <main>
+    
+    fin.seekg(0,ios::beg);//starts the header search from the beginning of the file
+
+    fin.clear();//allows the file to be read through multiple times
+    
+    while(getline(fin, strTemp, '\n')){//while loop reads file til it finds the correct area heading
+
+        if(strTemp==strArea){
+ 
+            getline(fin,area.strDescription,'*');//if it finds the correct area heading, it reads its info  til it hits the *	
+
+            // Read past the direction blocks (I.E. <north>) and store the room name for that direction
+            fin>>strTemp2>>area.strNorth;				
+            fin>>strTemp2>>area.strEast;				
+            fin>>strTemp2>>area.strSouth;				
+            fin>>strTemp2>>area.strWest;//it then read in the surrounding area titles by skipping their <area> descriptors				
+
+        
+            return;									
+        }
+    }
+}
+/**********************************************************/
+//*********************Move********************************/
+//******Function is called to move through the*************/
+//****game if there is an area in that direction***********/
+/**********************************************************/
+void Move(ifstream &fin,tArea &area, string strArea){
+    
+    if(strArea=="None"){//detects if there is no area in the direction inputted				
+	
+        cout<<"You are unable to travel that way\n";//displays error message and returns the function
+        return;									
+    }
+    else{
+        
+        area.strCurrentArea=strArea;// Sets the current area to the new one
+
+	GetAreaInfo(fin, area);	// Passes in the file pointer so the new area data is read	
+
+	DisplayArea(area);// Displays current area
+	
+    }
+}
+/**********************************************************/
+//*********************Input*******************************/
+//******Main game mechanic feature. Receives **************/
+//****user input and reacts to it accordingly to **********/
+//*******progress through the game*************************/
+/**********************************************************/
+int Input(ifstream &fin, tArea &area){
+    
+    string strInput="";//holds user input
+    
+    cout<<endl<<":";//displays prompt
+    cin>>strInput;//reads in the user input
+    
+    if(strInput=="look"){
+        DisplayArea(area);//calls function to give current area description
+    }
+    else if(strInput=="north"){
+        Move(fin,area,area.strNorth);//calls function to move north if possible
+    }
+    else if(strInput=="east"){
+        Move(fin,area,area.strEast);//calls function to move east if possible
+    }
+    else if(strInput=="west"){
+        Move(fin,area,area.strWest);//calls function to move west is possible
+    }
+    else if(strInput=="south"){//calls function to move south if possible
+        Move(fin,area,area.strSouth);
+    }
+    else if(strInput=="help"||strInput=="?"){//displays commands to user
+        cout<<"Commands: north east west south look quit help\n";
+    }
+    else if(strInput=="quit"){//ends game
+        cout<<"Game Over\n";
+        return QUIT;
+    }
+    else{//displays when invalid input is received
+        cout<<"Invalid input\n";
+    }
+    
+    return CONTINUE;
+}
+
---
+++ Project/nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=Cygwin_4.x_1-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin_4.x_1-Windows
+CND_ARTIFACT_NAME_Debug=project
+CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin_4.x_1-Windows/project
+CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin_4.x_1-Windows/package
+CND_PACKAGE_NAME_Debug=project.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin_4.x_1-Windows/package/project.tar
+# Release configuration
+CND_PLATFORM_Release=Cygwin_4.x_1-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/Cygwin_4.x_1-Windows
+CND_ARTIFACT_NAME_Release=project
+CND_ARTIFACT_PATH_Release=dist/Release/Cygwin_4.x_1-Windows/project
+CND_PACKAGE_DIR_Release=dist/Release/Cygwin_4.x_1-Windows/package
+CND_PACKAGE_NAME_Release=project.tar
+CND_PACKAGE_PATH_Release=dist/Release/Cygwin_4.x_1-Windows/package/project.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
