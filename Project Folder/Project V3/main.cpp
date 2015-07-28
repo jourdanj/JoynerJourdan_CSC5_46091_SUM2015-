eebd0d14249ae651882012f6611833d7adfe5eb2
jourdanj <jourdanjoyner@gmail.com>
2015-07-28 16:05:56 -0700

    Signed-off-by: jourdanj <jourdanjoyner@gmail.com>

--- Project Folder/Project V3/main.cpp
+++ Project Folder/Project V3/main.cpp
@@ -9,14 +9,18 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <algorithm>
 using namespace std;
 
 #define GAME_FILE "escape_the_house.txt"//read in for the world data
 #define CONTINUE    1
 #define QUIT            0
+
+//global constants
+//const int COUNT=10;//#of items possible to fit in inventory
 //user libraries
 struct Area{//structure that holds the info for the main area
-
+    
     string strCurrentArea;//name of current area
     string strDescription;//description of current area
     string strNorth;//name of area to the north
@@ -25,21 +29,26 @@ struct Area{//structure that holds the info for the main area
     string strWest;//name of area to the west 
     string strView;//name of item to view
     string strViewOut;//name of the item to view
+    string strInventory/*[COUNT*/;//Inventory of items
 };
-//global constants
-
 //function prototypes
 void DisplayArea(Area &area);
 void GetAreaInfo(ifstream &fin,Area &area);
 void Move(ifstream &fin,Area &area,string);
-void GetView(ifstream &fin,Area &area,string);
 int Input(ifstream &fin,Area &area);
+void GetView(ifstream &fin,Area &area,string);
+void DisplayView(Area &area);
+void Safe(Area &area);
+void InventorySearch(Area &area);
+void InventorySort(Area &area);
+void DisplayInventory(Area &area);
+
 //execution
 int main(){
 
     ifstream fin;//pointer that opens and reads from file
     Area area;//area structure data
-    string dump="";
+    
     fin.open(GAME_FILE);
 
     if(fin.fail()){
@@ -123,18 +132,43 @@ void Move(ifstream &fin,Area &area, string strArea){
     if(strArea=="Locked"){
         
         cout<<"This door is locked.\n";//Displays error message if a locked door is approached
+        if(area.strInventory=="key"){
+         
+            cout<<"You use the key to unlock the door.\n";
+            
+            strArea="Office";// Sets the current area to the new one
+
+            GetAreaInfo(fin, area);	// Passes in the file pointer so the new area data is read	
+
+            DisplayArea(area);// Displays current area
+        }
+                
+        return;
+    }
+    if(strArea=="Boarded"){
+        
+        cout<<"This door is boarded up tight, Not getting out this way.\n";//Displays error message if back door is approached
         return;
     }
     if(strArea=="Safe2"){
-       area.strCurrentArea=strArea;// Sets the current area to the new one
+       
+        Safe(area);
 
 	GetAreaInfo(fin, area);	// Passes in the file pointer so the new area data is read	
 
 	DisplayArea(area);// Displays current area
         
-        cout<<"The End.\n";
 	
-        return 1;
+    }
+    if(strArea=="Window"){//final room in game
+        
+        area.strCurrentArea=strArea;// Sets the current area to the new one
+
+	GetAreaInfo(fin, area);	// Passes in the file pointer so the new area data is read	
+
+	DisplayArea(area);// Displays current area
+        
+        exit(1);
     }
     else{
         
@@ -147,17 +181,6 @@ void Move(ifstream &fin,Area &area, string strArea){
     }
 }
 /**********************************************************/
-//*********************GetView*****************************/
-//************************Receives ************************/
-//****user input and reacts to it accordingly to **********/
-//*******look at an object in game in detail***************/
-/**********************************************************/
-void GetView(ifstream &fin, Area &area,string strInput){
-    
-    string vArea="<"+area.strCurrentArea+"|"+strInput+">";
-    
-}
-/**********************************************************/
 //*********************Input*******************************/
 //******Main game mechanic feature. Receives **************/
 //****user input and reacts to it accordingly to **********/
@@ -165,7 +188,8 @@ void GetView(ifstream &fin, Area &area,string strInput){
 /**********************************************************/
 int Input(ifstream &fin, Area &area){
     
-    string strInput="";//holds user input
+    string strInput="",
+           strInput2="";//holds user input
     
     cout<<endl<<":";//displays prompt
     cin>>strInput;//reads in the user input
@@ -175,8 +199,9 @@ int Input(ifstream &fin, Area &area){
     }
     else if(strInput=="view"){
         cout<<"What would you like to view?\n";
-        cin>>strInput;
-
+        cin>>strInput2;
+        GetView(fin,area,strInput2);
+        DisplayView(area);
     }
     else if(strInput=="north"){
         Move(fin,area,area.strNorth);//calls function to move north if possible
@@ -203,4 +228,97 @@ int Input(ifstream &fin, Area &area){
     
     return CONTINUE;
 }
+/**********************************************************/
+//*********************GetView*****************************/
+//************************Receives ************************/
+//****user input and reacts to it accordingly to **********/
+//*******look at an object in game in detail***************/
+/**********************************************************/
+void GetView(ifstream &fin, Area &area,string strInput2){
+    
+    string strV_Area="<"+area.strCurrentArea+"|"+strInput2+">";
+    string strLine="";
+    fin.seekg(0,ios::beg);//starts the header search from the beginning of the file
 
+    fin.clear();//allows the file to be read through multiple times
+    
+    while(getline(fin, strLine, '\n')){//while loop reads file til it finds the correct area heading
+        fin>>strLine;
+        if(strLine==strV_Area){
+
+            getline(fin,area.strViewOut,'*');//if it finds the correct area heading, it reads its info  til it hits the *
+            
+         
+        }
+    }
+}
+/**********************************************************/
+//*****************DisplayView*****************************/
+//******Function is called to display strViewOut***********/
+/**********************************************************/
+void DisplayView(Area &area)
+{
+    cout<<area.strViewOut<<endl;//displays view description		
+}
+/**********************************************************/
+//********************Safe*********************************/
+//******Function is called to interact with the safe*******/
+/**********************************************************/
+void Safe(Area &area){
+    
+    const int CODE=4;
+    int code[4];
+    cout<<"Enter the 4 digit combination.\n";//code is learned by solving puzzles on select view items in game
+    
+    for(int i=0;i<CODE;i++){
+        
+       if(i==0){
+           cout<<"Enter "<<i+1<<"st digit.\n";//output to request code input
+       }
+       else if(i==1){
+           cout<<"Enter "<<i+1<<"nd digit.\n";
+       }
+       else if(i==2){
+           cout<<"Enter "<<i+1<<"rd digit.\n";
+       }else if(i==3){
+           cout<<"Enter "<<i+1<<"th digit.\n";
+       }
+        cin>>code[i];
+    }
+    if(code[0]==8 && code[1]==4 && code[2]==8 && code[3]==5){//correct code received.
+        cout<<"SUCCSESS! You have managed to open the vault!\n";
+        cout<<"You retrieved the key from the safe.\n";
+        area.strInventory="key";
+        
+    }
+    else{
+        "Incorrect combination.\n";
+    }
+    
+}
+/**********************************************************/
+//*********************DisplayInventory********************/
+//*********************Outputs Inventory*******************/
+/**********************************************************/
+/*void DisplayInventory(Area &area){
+    for(int i=0;i<COUNT;i++){
+        cout<<area.strInventory[i]<<endl;  
+    }
+}*/
+/**********************************************************/
+//*********************InventorySort*********************/
+//******Function is called to interact with the safe*******/
+/**********************************************************/
+/*void InventorySort(Area &area){
+            
+    int temp=-1,        
+        end=9;
+    
+    int z=sizeof(Area.strInventory)/sizeof(Area.strInventory[0]); //Get the array size
+
+    sort(Area.strInventory,Area.strInventory+z); //Use the start and end like this
+
+    for(int y=0;y<z;y++){
+        cout << Area.strInventory[y] << endl;
+    }
+}*/
